114785467cc0f52e873a5b3ef8467f7e4dc8ca63
rahul <ra_mhul@rediffmail.com>
2011-07-23 09:37:06 +0530

    1.Machine summary report --refresh problem 2.Target-achive percentage calculation along with coloring , made generic fun @ app_helper Formula applied - percent_target = ((pre_month_avg_hc.to_f/target.to_f)*100).round to calculate.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,34 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def previous_month_avg_hc(cluster_name,shop_name,date)
+    date = date.to_s
+    first_date,last_date = get_last_months_last_date(date)
+    avg = Counterdata.average(:HC,:conditions=>["clustername=? and shopname=? and date>=? and date<=?",cluster_name,shop_name,first_date,last_date]).round
+  end
+
+  def target_percentage(pre_month_avg_hc,target)
+    #raise target.to_i.inspect
+    percent_target = ((pre_month_avg_hc.to_f/target.to_f)*100).round
+    tar = 100 - percent_target
+    target_achive = tar.abs
+    per = '%'
+    percent_font_color(pre_month_avg_hc,target,target_achive,per)
+  end
+
+  def percent_font_color(first_value,second_value,result,per)
+    if first_value.to_i < second_value.to_i
+      return ("<span style='color:#f00;font-weight:bold;'>"+ result.to_s + per.to_s + "</span>")
+    else
+      return ("<span style='color:blue;font-weight:bold;'>" + result.to_s + per.to_s  + "</span>")
+    end
+  end
+
+  def get_last_months_last_date(date)
+    last_date = (Date.parse(Date.parse(date.to_s).strftime('%Y-%m-01'))-1).to_s
+    first_date = Date.parse(last_date).strftime('%Y-%m-01')
+    return first_date,last_date
+  end
+
 end
+
+
