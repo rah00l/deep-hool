0d14ec625a2c06ec7f3c6d505b7347bcc89437cd
rahul <ra_mhul@rediffmail.com>
2011-07-30 15:11:51 +0530

    Short Extra sum0f done for all report

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,18 +38,30 @@ module ApplicationHelper
     #abc.blank? ? Shop.find_by_ShopName(shop_name).os : Counterdata.find(:first,:conditions=>["shopname=? and date=?",shop_name,prev_date]).os
   end
 
-  def short_extra_values(date,cluster_name,shop_name,group_id)
-    se = ShortExtra.find_by_date_and_cluster_name_and_shop_name_and_group_id(date,cluster_name,shop_name,group_id)
-    se.blank? ? '' : se.short_extra
+  def short_extra_values(date1,date2,cluster_name,shop_name,group_id)
+    se = ShortExtra.sum(:short_extra,:conditions => ["cluster_name=? and shop_name=? and group_id=? and Date>=? and Date<=? ",cluster_name,shop_name,group_id,date1,date2])
   end
 
   def font_color(number)
-    number < 0 ?  ("<span style='color:#f00;font-weight:bold;'>"+ (number.abs).to_s + "</span>") : ("<span style='color:blue;font-weight:bold;'>" + (number.abs).to_s  + "</span>")
+    number < 0 ?  ("<span style='color:#f00;font-weight:bold;'>"+ (number).to_s + "</span>") : ("<span style='color:blue;font-weight:bold;'>" + (number).to_s  + "</span>")
   end
 
   def sum_of_values_for_all_group(money_type,shopname,date1,date2)
     Counterdata.sum("#{money_type}",:conditions => ["ClusterName=? and Date>=? and Date<=? ",shopname,date1,date2])
   end
+
+  def tot_percentage_color(number)
+    if number.to_i <=59
+      return ("<span style='color:red;font-weight:bold;'>" +number.to_s + '%' +"</span>")
+    elsif number.to_i >59 and number.to_i <=69
+      return ("<span style='color:blue;font-weight:bold;'>" + number.to_s + '%'+ "</span>")
+    elsif number.to_i >69 and number.to_i <=80
+      ("<span style='color:green;font-weight:bold;'>"+ number.to_s + '%' +"</span>")
+    elsif number.to_i >=81
+      return ("<span style='color:red;font-weight:bold;'>"+ number.to_s + '%' + "</font>")
+    end
+  end
+
 
 end
 
