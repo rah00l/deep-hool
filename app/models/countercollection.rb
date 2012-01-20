f445027f2866b9d7158c2dabd3576f5a586310ad
unknown <sandeep@.(none)>
2012-01-20 10:56:56 +0530

    Counter-Collection has issue with duplicate entries which is get resolved + added presence of for HC and Outstanding for preventing NULL value insertion.

--- app/models/countercollection.rb
+++ app/models/countercollection.rb
@@ -1,2 +1,6 @@
 class Countercollection < ActiveRecord::Base
+  validates_uniqueness_of :Date , :scope => [:ClusterName,:ShopName],:message=> "Duplicate Entry"
+  validates_presence_of :HC,:Outstanding
 end
+
+
