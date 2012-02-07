f42d03954003bbc6bfcbd2680fc1cbe927b20f72
rahul <ra_mhul@rediffmail.com>
2012-02-07 23:32:40 +0530

    1.HC daily + monthaly / 10 2.Counter-collection one more condition get added + 500 server end validation added

--- app/models/clientdata.rb
+++ app/models/clientdata.rb
@@ -1,9 +1,3 @@
 class Clientdata < ActiveRecord::Base
 validates_uniqueness_of :Date , :scope => [:GroupID,:ShopName,:MachineNo,:MachineName],:message=> "Duplicate Entry"
-#validates_presense_of  :GroupID,:ShopName,:MachineNo,:MachineName
-#
-#   with_options :if => :driver? do |driver|
-#    driver.validates_presence_of :truck_serial
-#    driver.validates_length_of :truck_serial, :maximum => 30
-#  end
 end
--- app/models/countercollection.rb
+++ app/models/countercollection.rb
@@ -1,6 +1,15 @@
 class Countercollection < ActiveRecord::Base
   validates_uniqueness_of :Date , :scope => [:ClusterName,:ShopName],:message=> "Duplicate Entry"
   validates_presence_of :HC,:Outstanding
+  validate :check_os_in_multiples_of_five_hundred
+
+
+  def check_os_in_multiples_of_five_hundred
+    if self.Outstanding.to_i % 500 != 0
+      self.errors.add_to_base("PLEASE ENTER AMOUNT IN MULTIPLES OF  0  OR  500 ")
+    end
+  end
+  
 end
 
 
