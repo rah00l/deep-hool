d8e9e87aa9027a3de8472fe4f73cb0fa32d0ae70
rahul <ra_mhul@rediffmail.com>
2013-02-12 00:16:37 +0545

    home path added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.home '',:controller => 'account', :action => 'login'
   # The priority is based upon order of creation: first created -> highest priority.
   
   # Sample of regular route:
