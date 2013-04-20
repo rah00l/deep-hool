1d58213ac0c7606eba82e9f2f3c95fee011c8594
rahul <rahul@rahul-Satellite-L510.(none)>
2013-04-21 01:29:53 +0530

    machine_master got added + rake to map with existing machines data, "rake add_data_into_machine_master:start_adding"

--- app/helpers/machines_helper.rb
+++ app/helpers/machines_helper.rb
@@ -2,4 +2,8 @@ module MachinesHelper
   def get_machine_types
     MachineType.find(:all).collect {|mt| [mt.name,mt.id]}
   end
+
+  def get_machine_names
+    MachineMaster.find(:all).collect {|mt| [mt.short_name,mt.short_name]}
+  end
 end
